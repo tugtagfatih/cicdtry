0d5b1c1a57be045957e5071efac8220dad6dbbb2
tugtagfatih <117814951+tugtagfatih@users.noreply.github.com>
2024-10-13 18:15:03 +0300

    Update index.js

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
   res.end('Hello World\n');
+  res.end(`Server running at http://${hostname}:${port}/`);
 });
 
 server.listen(port, hostname, () => {
